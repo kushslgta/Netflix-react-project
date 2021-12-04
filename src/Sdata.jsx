948832b3593c4c96183322eae9d20a7e6088520b
kushal <kushalagrawal778@gmail.com>
2021-12-04 11:08:52 +0530

    Initial commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,28 @@
+const Sdata=[
+    { 
+        id:1,
+    sname:"DARK",
+    imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+    title:"A Netflix Original Series",
+    link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {  id:2,
+        sname:"Stranger Things",
+        imgsrc : "https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+        },
+        {  id:3,
+            sname:"DARK",
+            imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+            title:"A Netflix Original Series",
+            link:"https://www.netflix.com/in/title/80990668?source=35"
+            },
+            {  id:4,
+                sname:"Stranger Things",
+                imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+                title:"A Netflix Original Series",
+                link:"https://www.netflix.com/in/title/80990668?source=35"
+                },
+];
+export default Sdata;
